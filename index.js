e7e77ac96674776b616c7997780525b1f3996357
Zowie Erickson <zowie.erickson@gmail.com>
2022-05-01 22:21:09 -0400

    created books css, added book title from books.json

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('hi zowie')
+async function getBooks() {
+    const response = await fetch(`books.json`);
+    const data = await response.json();
+    return data;
+}
+
+function displayBooksHtml(book) {
+    return `
+    <div class="book">
+        <div class="book-spine"></div>
+        <div class="book-title">${book.title}</div>
+        <div class="book-footer"></div>
+    </div>
+
+    `
+}
+
+getBooks().then(books => {
+    console.log(books)
+    document.body.innerHTML = `
+    <div class="books-container">
+    ${books.map(book => displayBooksHtml(book)).join('')}
+    </div>
+    `;
+})
